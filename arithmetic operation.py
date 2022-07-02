203cb63cf533bd50ebad59623997099a1b2a59b5
SPKul <91112742+SPKul@users.noreply.github.com>
2022-07-02 13:19:39 +0530

    arithmatic operatio0 ++

---
+++ arithmetic operation.py
@@ -0,0 +1,20 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 19:28:18) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> Type "copyright", "credits" or "license()" for more information.
+SyntaxError: invalid syntax
+>>> a=2
+>>> b=2
+>>> print(a+b)
+4
+>>> print(a-b)
+0
+>>>  print(a-b)
+ 
+SyntaxError: unexpected indent
+>>> print(a*b)
+4
+>>> print(a/b)
+1.0
+>>> print(a%b)
+0
+>>> 
